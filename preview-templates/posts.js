c5a6387112174c222b14df05ae6043fa20fb7fe3
Todd <ihatespam@gmail.com>
2023-02-24 00:48:56 -0600

    Update posts.js

--- preview-templates/posts.js
+++ preview-templates/posts.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 const BlogPreview = ({ entry }) => {
+  console.log(entry.toJS())
   const title = entry.getIn(["data", "title"])
   const date = entry.getIn(["data", "date"])
   const body = entry.getIn(["data", "body"])
