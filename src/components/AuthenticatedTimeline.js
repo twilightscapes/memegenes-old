fff0b2e7f71e78aaab60f406fea0acfad3474020
Todd <ihatespam@gmail.com>
2023-04-25 06:55:25 -0500

    working timeline with favs saved to local

--- src/components/AuthenticatedTimeline.js
+++ src/components/AuthenticatedTimeline.js
@@ -79,9 +79,11 @@ const AuthenticatedTimeline = () => {
       // If the item is already in favorites, remove it
       const index = newFavorites.findIndex((favorite) => favorite.link === item.link);
       newFavorites.splice(index, 1);
+      item.favorite = false;
     } else {
       // Otherwise, add the item to favorites
       newFavorites.push(item);
+      item.favorite = true;
     }
   
     setFavorites(newFavorites);
@@ -92,7 +94,7 @@ const AuthenticatedTimeline = () => {
     // Update the favorite status of the item in the feed
     const newFeed = feed.map((feedItem) => {
       if (feedItem.link === item.link) {
-        return { ...feedItem, favorite: !feedItem.favorite };
+        return { ...feedItem, favorite: item.favorite };
       }
       return feedItem;
     });
@@ -128,36 +130,60 @@ const AuthenticatedTimeline = () => {
 
           {combinedFeed.slice(0, visibleItems).map((item, index) => (
           
-          <div className="post-card1" style={{ justifyContent: "center", alignItems: "center" }} key={index}>
+
+          <div className="post-card1" style={{ justifyContent: "center", alignItems: "center", position:'relative' }} key={index}>
+
+
+
+
   <a className="postlink" href={item.link} rel="noopener noreferrer">
     {item.imageUrl && (
       <img className="featured-image1" src={item.imageUrl} alt={item.title} style={{ position: 'relative', zIndex: '1', maxHeight: '', margin: '0 auto' }} />
     )}
 
     <div className="post-content" style={{display:'flex', flexDirection:'column', justifyContent:'center', width:'100%', height:'', position:'relative', background:'', padding:'0', margin:'0 auto 0 auto', textAlign:'center', overFlow:'hidden'}}>
-      <div className="panel" style={{display:'flex', justifyContent:'space-between', alignItems:'center', margin:'10px auto', maxWidth:'80vw', gap:'.4vw', height:'', textAlign:'center', padding:'1vh 2vw', fontSize:'clamp(1rem, 1vw, 1rem)',  background:'rgba(0, 0, 0, 0.7)', borderRadius:'', color:'#aaa' }}>
+      
+      <div className="panel" style={{display:'flex', flexDirection:'column', justifyContent:'space-between', alignItems:'center', margin:'10px auto', maxWidth:'80vw', gap:'.4vw', height:'', textAlign:'center', padding:'1vh 2vw', fontSize:'clamp(1rem, 1vw, 1rem)',  background:'rgba(0, 0, 0, 0.7)', borderRadius:'', color:'#aaa' }}>
       {/* <h2 onClick={() => toggleFavorite(item)}>
   {item.favorite ? "⭐" : "☆"} {item.name} - {item.title}
 </h2> */}
 
-<span className="star-button">
+
+{!item.favorite && (
   <button
+    className="star-button"
     onClick={(event) => {
       event.preventDefault();
       toggleFavorite(item);
     }}
-    style={{ cursor: "pointer", background: "none", border: "none" }}
+    style={{ cursor: "pointer", background: "none", border: "none", position:'absolute', bottom:'10px', right:'10px', zIndex:'2' }}
   >
-    {item.favorite ? "⭐" : "☆"}
+    ☆
   </button>
-</span>
+)}
+{item.favorite && (
+  <button
+    className="star-button"
+    onClick={(event) => {
+      event.preventDefault();
+      toggleFavorite(item);
+    }}
+    style={{ cursor: "pointer", background: "none", border: "none", position:'absolute', bottom:'10px', right:'10px', zIndex:'2' }}
+  >
+    ⭐
+  </button>
+)}
 
 
 
-<h2>
+
+
+
+
+<h2 style={{textAlign:'left', textWrap:'balance'}}>
   {item.name} - {item.title}
 </h2>
-        <p>{item.description}</p>
+        <p style={{textAlign:'left', textWrap:'balance', fontSize:'85%'}}>{item.description}</p>
       </div>
 
       {showDates ? (
