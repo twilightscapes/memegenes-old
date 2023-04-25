17698861e5868a09f436859630b67646b811804a
Todd <ihatespam@gmail.com>
2023-04-25 03:45:48 -0500

    Update AuthProvider.js

--- src/AuthProvider.js
+++ src/AuthProvider.js
@@ -1,27 +1,31 @@
-// src/AuthProvider.js
-
 import React, { createContext, useState, useEffect } from "react";
 import netlifyIdentity from "netlify-identity-widget";
-import { navigate } from "gatsby";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [fullName, setFullName] = useState('Unknown');
 
-useEffect(() => {
-  netlifyIdentity.init();
-  console.log(netlifyIdentity); // add this line
-  setUser(netlifyIdentity.currentUser());
-  netlifyIdentity.on("login", (user) => setUser(user));
-  netlifyIdentity.on("logout", () => setUser(null));
-}, []);
+  useEffect(() => {
+    netlifyIdentity.init();
+    setUser(netlifyIdentity.currentUser());
+    netlifyIdentity.on("login", (user) => {
+      setUser(user);
+      setFullName(user?.user_metadata?.full_name || 'Unknown');
+    });
+    netlifyIdentity.on("logout", () => {
+      setUser(null);
+      setFullName('Unknown');
+    });
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ user, netlifyIdentity }}>
+    <AuthContext.Provider value={{ user, netlifyIdentity, fullName }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
 
 export { AuthContext, AuthProvider };
